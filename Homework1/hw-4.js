d124e9bb4093c156adef40a0501fe15499b00b5b
TenkoMor <liza.kortikova@yandex.ru>
2023-06-02 12:54:38 +0300

    On branch hw5  Changes to be committed: 	modified:   Homework1/_mobile.scss 	modified:   Homework1/_tablet.scss 	new file:   Homework1/hw-4.js 	new file:   Homework1/img/undraw_video_game_night_8h8m.png 	new file:   Homework1/img/undraw_video_game_night_8h8mmob.png 	modified:   Homework1/index.html 	modified:   Homework1/style.css 	modified:   Homework1/style.css.map 	modified:   Homework1/style.scss

---
+++ Homework1/hw-4.js
@@ -0,0 +1,104 @@
+//task1
+const less = (a, b) => {
+    if (a >= b) {
+        return b
+    } else {
+        return a
+    }
+}
+console.log(less(2, 6))
+
+//task2
+const evenOdd  = (a) => {
+    if (a % 2 == 0) {
+        return "Число чётное"
+    } else {
+        return "Число нечётное"
+    }
+}
+console.log(evenOdd(7))
+
+//task3.1
+const square1 = (a) => console.log(a**2)
+square1(2)
+
+//task3.2
+const square2 = (b) => {
+    let result = b**2 
+    return result
+}
+console.log(square2(4))
+
+//task4 
+const sayHello = (a = prompt("Сколько вам лет?")) => {
+    if ((a >= 0) && (a <= 12)) {
+        console.log("Привет, друг!")
+    } else if (a >= 13) {
+        console.log("Добро пожаловать!")
+    } else {
+        console.log("Вы ввели неправильное значение")
+    }
+}
+sayHello()
+
+//task5
+const check = (a, b) => {
+    let num1 = isNaN(a)
+    let num2 = isNaN(b)
+    if ((num1 == false) && (num2 == false)) {
+        let result = a*b
+        return result 
+    } else {
+        return "Одно или оба значения не являются числом"
+    }
+}
+console.log(check(3, "rr"))
+
+//task6
+const cube = (a = prompt("Сколько вам лет?")) => {
+    if (isNaN(a)) {
+        return 'Переданный параметр не является числом'
+    } else {
+        let result = a**3
+        return `${a} в кубе равняется ${result}`
+    }
+}
+for (let c = 0; c <= 10 ; c++) {
+    console.log(cube(c))
+}
+
+//task7
+function getRectangleArea() {
+    return Math.PI * (this.radius**2)
+}
+function getRectanglePerimeter() {
+    return  2 * Math.PI * this.radius
+}
+const circle1 = {
+    radius: 10, 
+    getArea: getRectangleArea,
+    getPerimeter: getRectanglePerimeter
+}
+const circle2 = {
+    radius: 12,
+    getArea: getRectangleArea,
+    getPerimeter: getRectanglePerimeter
+}
+console.log(circle2.getArea())
+console.log(circle2.getPerimeter())
+
+//task8
+const month = (monthNumber = Number(prompt("Введит номер месяца"))) => {
+    if ((monthNumber == 12) || (monthNumber >= 1) && (monthNumber <= 2)){
+        return "Зима" 
+    } else if ((monthNumber >= 3) && (monthNumber <= 5)){
+        return "Весна"
+    } else if ((monthNumber >= 6) && (monthNumber <= 8)){
+        return "Лето"
+    } else if ((monthNumber >= 9) && (monthNumber <= 11)){
+        return "Осень"
+    } else {
+        return "Такого месяца не существует"
+    }
+}
+
